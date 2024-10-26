74c905d6e56f285c2cfbac439175331f74f6174f
uralbek1 <162686008+uralbek1@users.noreply.github.com>
2024-10-26 21:58:32 +0500

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText;
+     document.getElementById("index").innerHTML = this.responseText;
     }
   };
   xhttp.open("GET", "https://dom1-uralbek.netlify.app/index.txt", true);
